3dfa0ff58a824babc490ec59872369a1cbca9d86
Tetiana <tetianap@stanford.edu>
2022-10-13 10:04:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="osbdo",
-    version="0.0.4",
+    version="0.0.5",
     packages=["osbdo"],
     license="GPLv3",
     description="Oracle-Structured Bundle Distributed Optimization",
